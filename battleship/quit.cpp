a9e83dd16dd5c99048e464953583bf7f8c7319b5
meganhqh <85163044+meganhqh@users.noreply.github.com>
2021-12-02 23:24:41 +0800

    Update quit.cpp

--- battleship/quit.cpp
+++ battleship/quit.cpp
@@ -17,7 +17,7 @@ using namespace std;
 // Input: quit? , save?
 // Output: return to the game when user decides not to quit
 // Output: modify the savefile.txt if user saves
-bool Quit(int quit_sequence, int difficulty, int start_player, int indicator, int ship_inputted, char matrix[6][6], char matrixComp[6][6], char recording[6][6], char recordingComp[6][6], string ship_position[3][3], string ship_positionComp[3][3], string *&user_attack, string *&comp_attack, int count_user, int comp_ship_left_last){
+bool Quit(int quit_sequence, int difficulty, int start_player, int indicator, int ship_inputted, char matrix[6][6], char matrixComp[6][6], char recording[6][6], char recordingComp[6][6], string ship_position[3][3], string ship_positionComp[3][3], string *&user_attack, string *&comp_attack, int count_user, int comp_ship_left_last, bool hunt, bool target_N, bool target_S, bool target_E, bool target_W){
   int quit, save;
   cout << endl << "Are you sure you want to quit?" << endl;
   cout << "1 - Yes" << endl;
@@ -47,7 +47,7 @@ bool Quit(int quit_sequence, int difficulty, int start_player, int indicator, in
   // save difficulty level, indicator (player sequence), ship_inputted, quit sequence,
   // ships location board, recording board, previous attack positions,number of times that user/ computer attacks
   if (save == 1) {
-    SaveFilePlacement(quit_sequence, difficulty, start_player, indicator, ship_inputted, matrix, matrixComp, recording, recordingComp, ship_position, ship_positionComp, user_attack, comp_attack, count_user, comp_ship_left_last);
+    SaveFilePlacement(quit_sequence, difficulty, start_player, indicator, ship_inputted, matrix, matrixComp, recording, recordingComp, ship_position, ship_positionComp, user_attack, comp_attack, count_user, comp_ship_left_last, hunt, target_N, target_S, target_E, target_W);
   }
   // close the program
   if (quit == 1){
