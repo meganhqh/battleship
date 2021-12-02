b166cf6e88b111d652231579b7112b033386191b
sysoo <55819313+sysoo@users.noreply.github.com>
2021-12-03 02:37:48 +0800

    Update check_winner.h
    Change code comment

--- battleship/check_winner.h
+++ battleship/check_winner.h
@@ -4,10 +4,7 @@
 
 #include <string>
 
-
 // Check who is the winner by eliminating from the dynamic array according to the attack position
-// Input: valid attacking position, indicator (whose turn to play the game)
-// Output: winner in the game?
 int CheckWinner(std::string position, int indicator);
 
 #endif
