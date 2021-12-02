332cd68d4bf20c4af2800a19e6976bea3d50205a
meganhqh <85163044+meganhqh@users.noreply.github.com>
2021-12-03 00:14:30 +0800

    Update save_load.h

--- battleship/save_load.h
+++ battleship/save_load.h
@@ -8,7 +8,7 @@
 // Input: difficulty level, indicator (player sequence), ship_inputted, quit sequence
 // Input: ships location board, recording board, previous attack positions
 // Input: number of times that user/ computer attacks
-void SaveFilePlacement(int quit_sequence, int difficulty, int start_player, int indicator, int ship_inputted, char matrix[6][6], char matrixComp[6][6], char recording[6][6], char recordingComp[6][6], std::string ship_position[3][3], std::string ship_positionComp[3][3], std::string *&user_attack, std::string *&comp_attack, int count_user, int comp_ship_left_last, bool hunt, bool target_N, bool target_S, bool target_E, bool target_W);
+void SaveFilePlacement(int quit_sequence, int difficulty, int start_player, int indicator, int ship_inputted, char matrix[6][6], char matrixComp[6][6], char recording[6][6], char recordingComp[6][6], std::string ship_position[3][3], std::string ship_positionComp[3][3], std::string *&user_attack, std::string *&comp_attack, int count_user, int count_comp, int comp_ship_left_last, bool hunt, bool target_N, bool target_S, bool target_E, bool target_W);
 // Check the validity of the savefile.txt when loading
 int load_test ();
 // Load the game progress from savefile.txt but checking function is removed
@@ -16,6 +16,6 @@ int load_test ();
 // Input: ships location board, recording board, previous attack positions
 // Input: number of times that user/ computer attacks
 // Output: modify all inputs based on savefile.txt
-void load_file (int &quit_sequence, int start_player, int &difficulty, int &indicator,  int &ship_inputted, char matrix[6][6], char matrixComp[6][6], char recording[6][6], char recordingComp[6][6], std::string ship_position[3][3], std::string ship_positionComp[3][3], std::string *&user_attack, std::string *&comp_attack, int &count_user, int &comp_ship_left_last, bool hunt, bool target_N, bool target_S, bool target_E, bool Target_W);
+void load_file (int &quit_sequence, int start_player, int &difficulty, int &indicator,  int &ship_inputted, char matrix[6][6], char matrixComp[6][6], char recording[6][6], char recordingComp[6][6], std::string ship_position[3][3], std::string ship_positionComp[3][3], std::string *&user_attack, std::string *&comp_attack, int &count_user, int &count_comp, int &comp_ship_left_last, bool &hunt, bool &target_N, bool &target_S, bool &target_E, bool &target_W);
 
 #endif
