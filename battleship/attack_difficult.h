15e9cec053c36b57d1794de5b2a4ef4e79c4971a
sysoo <55819313+sysoo@users.noreply.github.com>
2021-12-03 02:36:34 +0800

    Update attack_difficult.h

--- battleship/attack_difficult.h
+++ battleship/attack_difficult.h
@@ -3,8 +3,6 @@
 #define ATTACK_DIFFICULT_H
 
 // Check the error for input ship location
-// Input: input row, column, director and flag_user (determine whose turn to play)
-// Output: false for invalid positions
 bool CompTarget(int row, int column);
 
 // Set the location to attack ships
