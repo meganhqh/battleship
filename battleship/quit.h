27aba4eb39486df5640bc6f21bcea150e09a8747
meganhqh <85163044+meganhqh@users.noreply.github.com>
2021-12-03 00:11:50 +0800

    Update quit.h

--- battleship/quit.h
+++ battleship/quit.h
@@ -5,6 +5,6 @@
 #include <string>
 
 
-bool Quit(int quit_sequence, int difficulty, int start_player, int indicator, int ship_inputted, char matrix[6][6], char matrixComp[6][6], char recording[6][6], char recordingComp[6][6], std::string ship_position[3][3], std::string ship_positionComp[3][3], std::string *&user_attack, std::string *&comp_attack, int count_user, int comp_ship_left_last, bool hunt, bool target_N, bool target_S, bool target_E, bool target_W);
+bool Quit(int quit_sequence, int difficulty, int start_player, int indicator, int ship_inputted, char matrix[6][6], char matrixComp[6][6], char recording[6][6], char recordingComp[6][6], std::string ship_position[3][3], std::string ship_positionComp[3][3], std::string *&user_attack, std::string *&comp_attack, int count_user,int count_comp, int comp_ship_left_last, bool hunt, bool target_N, bool target_S, bool target_E, bool target_W);
 
 #endif
