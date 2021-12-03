4b92bb7ca0e24d60864131bda2d07acef0ac805d
meganhqh <85163044+meganhqh@users.noreply.github.com>
2021-12-03 11:31:59 +0800

    Update attack_difficult.h
    Difficulty level = Hard

--- attack_difficult.h
+++ attack_difficult.h
@@ -3,9 +3,14 @@
 #define ATTACK_DIFFICULT_H
 
 // Check the error for input ship location
+// Check the 4 direction surrounded the attacked ship
+// Input: valid row and column
+// Output: true if there is ship at the specific row and column for targetting, othewise false
 bool CompTarget(int row, int column);
 
 // Set the location to attack ships
+// Inputs: difficulty level, indicator (whose turn to play the game), load (if we are loading from load file), and quit (to capture if user quits halfway)
+// Output: return 1 if quit halfway, else return 0; and successful and insuccessful of hunt and target
 int SetAttackDifficult(int difficulty, int indicator, int load, int &quit);
 
 
