54d5c5662cafde75e925626695645421e3357fbf
meganhqh <85163044+meganhqh@users.noreply.github.com>
2021-12-03 11:34:58 +0800

    Update attack_easy.h
    Difficulty level = easy

--- attack_easy.h
+++ attack_easy.h
@@ -3,6 +3,8 @@
 #define ATTACK_EASY_H
 
 // Set the location to attack ships
+// Inputs: difficulty level, indicator (whose turn to play the game), load (if we are loading from load file), and quit (to capture if user quits halfway)
+// Output: return 1 if quit halfway, else return 0
 int SetAttack(int difficulty, int indicator, int load, int &quit);
 
 
