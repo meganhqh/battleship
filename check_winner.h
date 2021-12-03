ea62274efd57d7ad085b8a6cb38e50fb79182eba
meganhqh <85163044+meganhqh@users.noreply.github.com>
2021-12-03 11:38:08 +0800

    Update check_winner.h
    Check winner of the game

--- check_winner.h
+++ check_winner.h
@@ -4,7 +4,9 @@
 
 #include <string>
 
-// Check who is the winner by eliminating from the dynamic array according to the attack position
+// Check who is the winner by determining the number of ships left in both player's board
+// Input: valid attacking position, indicator (whose turn to play the game)
+// Output: number of ships left in computer's board
 int CheckWinner(std::string position, int indicator);
 
 #endif
