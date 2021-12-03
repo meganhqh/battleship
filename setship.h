11aa5ae044d7be720cc1ecae637f83dcb631dd8b
sysoo <55819313+sysoo@users.noreply.github.com>
2021-12-03 12:21:45 +0800

    Update setship.h
    update function comment

--- setship.h
+++ setship.h
@@ -10,17 +10,16 @@
 // Output: false for invalid positions
 bool CheckError(int row, int column, char direction, int flag_user);
 
-// Change the relevant input position to 'S' and store them in a dynamic array
-// Input: vlid ship input position and direction
-// Output: modify the gameboard according to the valid row, column and direction
+// Change the relevant input position to 'S' on the ship placement or game board, and also store the coordinates in a ship position array
+// Input: a 6x6 grid, an array recording ship position coordinates, valid ship input position and direction
 void WriteShipPosition(char matrix[6][6], std::string ship_position[3][3],int ship_number, int row, int column, char direction);
 
-// Set the location of the ship at the early stage
-// Input: row, column and direction
-// Output: print out the gameboard for valid row, column and direction
+// Set the location of the ship in the first part of the game
+// Input: difficulty level, indicator (whose turn), load (whether or not we load previous game), quit (whether or not user quits)
+// Output: if user decides to quit, return 1, 0 otherwise
 int SetShips(int difficulty, int indicator, int load, int &quit);
 
-// Set random input by Computer
+// Set random ship position input by Computer
 void SetCompShips();
 
 #endif
